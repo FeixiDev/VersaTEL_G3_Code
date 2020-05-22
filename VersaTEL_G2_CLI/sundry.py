9520c766f063a863aed6f4325d5e4033f754e6f4
vince <iwyswx@163.com>
2020-05-22 13:51:13 +0800

    pep8 coding style specification

--- VersaTEL_G2_CLI/sundry.py
+++ VersaTEL_G2_CLI/sundry.py
@@ -1,13 +1,16 @@
-#coding:utf-8
+# coding:utf-8
 import socket
 
-#Connect to the socket server and transfer data, and finally close the connection.
+# Connect to the socket server and transfer data, and finally close the
+# connection.
+
+
 def send_via_socket(data):
     ip = "127.0.0.1"
     port = 12144
 
     client = socket.socket()
-    client.connect((ip,port))
+    client.connect((ip, port))
     judge_conn = client.recv(8192).decode()
     print(judge_conn)
     client.send(b'database')
@@ -16,5 +19,3 @@ def send_via_socket(data):
     client.recv(8192)
     client.send(b'exit')
     client.close()
-
-
